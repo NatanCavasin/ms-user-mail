e44522966d64b61c45f946a96c44a81d2c1b2136
Natan Cavasin <natancavasin@hotmail.com>
2024-07-29 09:46:52 -0300

    feat: created RabbitMQ configuration

--- user/src/main/java/com/ms/user/controllers/UserController.java
+++ user/src/main/java/com/ms/user/controllers/UserController.java
@@ -7,11 +7,12 @@ import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("user")
 public class UserController {
 
     final UserService userService;
@@ -20,11 +21,17 @@ public class UserController {
         this.userService = userService;
     }
 
-    @PostMapping("/users")
+    @GetMapping("/all")
+    public ResponseEntity<List<UserModel>> getAllUsers(){
+        return ResponseEntity.status(HttpStatus.OK).body(this.userService.getAllUsers());
+    }
+
+    @PostMapping("/create")
     public ResponseEntity<UserModel> saveUser(@RequestBody @Valid UserRecordDTO userRecordDTO){
         var userModel = new UserModel();
         BeanUtils.copyProperties(userRecordDTO, userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(this.userService.saveUser(userModel));
     }
+
 
 }
--- user/src/main/java/com/ms/user/services/UserService.java
+++ user/src/main/java/com/ms/user/services/UserService.java
@@ -6,6 +6,8 @@ import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserService {
 
@@ -13,6 +15,10 @@ public class UserService {
 
    public UserService(UserRepository userRepository){
        this.userRepository = userRepository;
+   }
+
+   public List<UserModel> getAllUsers(){
+       return this.userRepository.findAll();
    }
 
    @Transactional
